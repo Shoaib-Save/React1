0e6e542187b7d887fa7b8037f85c3fe258774afb
Shoaib-Save <metkarishoaib888@gmail.com>
2024-07-17 17:27:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Menu from './Menu';
+import About from './About'
+import Footer from './Footer';
+
+
+function App(){
+  return (
+    <>
+    
+    <Menu/>
+    <About/>
+    <Footer/>
+    </>
+  )
+}
+
+
+export default App;
+
